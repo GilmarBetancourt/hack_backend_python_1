4326990a3ecf84231bbcbe5d924f2378a794f50e
GilmarBetancourt <gilmarbeta@hotmail.com>
2025-06-03 20:29:52 -0400

    feat: hacks 1-6

--- app.py
+++ app.py
@@ -11,3 +11,40 @@ CORS(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+@app.route("/users", methods=["GET"])
+def hack_1():
+    return {'payload':'success'}
+
+
+@app.route("/user", methods=["POST"])
+def hack_2():
+    return {'payload':'success'}
+
+
+@app.route("/user", methods=["DELETE"])
+def hack_3():
+    return {'payload':'success'}
+
+#No sirve
+@app.route("/user", methods=["PUT"])
+def hack_4():
+    return {'payload':'success'}
+
+
+@app.route("/api/v1/users", methods=["GET"])
+def hack_5():
+    return {'payload':[]}
+
+
+#Hack 6
+@app.route("/api/v1/user", methods=["POST"])
+def hack_6():
+    email = request.args.get("email")
+    name = request.args.get("name")
+    payload = {
+        "email":email,
+        "name": name
+    }
+    return jsonify({"payload":payload})
