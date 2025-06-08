0441d468fd0aeacc577236e69df75f9b3c3baa20
GilmarBetancourt <gilmarbeta@hotmail.com>
2025-06-08 12:55:40 -0400

    feat: hacks 7 y 8

--- app.py
+++ app.py
@@ -12,27 +12,29 @@ CORS(app)
 if __name__ == "__main__":
     app.run(debug=True)
 
-
+#Hack 1
 @app.route("/users", methods=["GET"])
 def hack_1():
     return {'payload':'success'}
 
 
+#Hack 2
 @app.route("/user", methods=["POST"])
 def hack_2():
     return {'payload':'success'}
 
 
+#Hack 3
 @app.route("/user", methods=["DELETE"])
 def hack_3():
     return {'payload':'success'}
 
-#No sirve
+#Hack 4
 @app.route("/user", methods=["PUT"])
 def hack_4():
-    return {'payload':'success'}
+    return {'payload':'success', 'error':False}
 
-
+#Hack 5
 @app.route("/api/v1/users", methods=["GET"])
 def hack_5():
     return {'payload':[]}
@@ -48,3 +50,35 @@ def hack_6():
         "name": name
     }
     return jsonify({"payload":payload})
+
+#Hack 7
+@app.route("/api/v1/user/add", methods=["POST"])
+def hack_7():
+    email = request.form.get("email")
+    name = request.form.get("name")
+    id = request.form.get("id")
+    payload = {
+        'payload': {
+            'email':email,
+            'name':name,
+            'id':id,
+        }
+    }
+    return jsonify(payload)
+
+
+#Hack 8
+@app.route("/api/v1/user/create", methods=["POST"])
+def hack_8():
+    data = request.get_json()
+    email = data.get("email")
+    name = data.get("name")
+    id = data.get("id")
+    payload = {
+        'payload': {
+            'email':email,
+            'name':name,
+            'id':id,
+        }
+    }
+    return jsonify(payload)
